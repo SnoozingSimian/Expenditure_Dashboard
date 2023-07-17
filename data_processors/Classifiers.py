862e75f57ae0b341f6d02201f5d00e3d6ba2b747
SnoozingSimian <ayangofficial@gmail.com>
2023-07-17 15:18:24 +0530

    fix typo

--- data_processors/Classifiers.py
+++ data_processors/Classifiers.py
@@ -3,24 +3,25 @@ from abc import abstractmethod, ABC
 from transformers import pipeline
 import numpy as np
 
+
 class Classifer(ABC):
     def __init__(self) -> None:
         super().__init__()
         self._categories = set()
-        
+
     @property
     def categories(self):
         return self._categories
-    
+
     @categories.setter
     def categories(self, **kwargs):
         for k, v in kwargs.items():
             self._categories.add(v)
-    
+
     @categories.deleter
     def categories(self, category):
-        del self._categories.remove(category)
-        
+        self._categories.remove(category)
+
     @abstractmethod
     def classify(self, text):
         pass
@@ -30,9 +31,9 @@ class ZeroShotClassifer(Classifer):
     def __init__(self) -> None:
         super().__init__()
         self.pipe = pipeline(model="facebook/bart-large-mnli")
-        
+
     def classify(self, text):
         answer = self.pipe(text, candidate_labels=self._categories)
-        
-        i = np.argmax(answer['scores'])
-        return answer['labels'][i]
+
+        i = np.argmax(answer["scores"])
+        return answer["labels"][i]
